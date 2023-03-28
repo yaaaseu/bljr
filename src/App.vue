3c19d949aeb5333144d7ba362acd732a2b2ed85a
yaaaseu <129028929+yaaaseu@users.noreply.github.com>
2023-03-28 23:35:11 +0700

    2 files modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Adzkia Ketua" />
-    <tombol />
+    <HelloWorld msg="Hello"  />
+    <tombol class="btn btn-success"/>
+    
+  </div>
+
+  <div id="untuktelkom">
+  <telkom />
+  <img src="" alt="">
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import tombol from './components/tombol.vue';
+import telkom from './components/telkom.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     tombol,
-  },
-};
+    telkom,
+  }
+}
 </script>
 
 <style>
@@ -28,4 +39,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#untuktelkom {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin-top: 60px;
+}
+
+
 </style>
---
+++ src/components/telkom.vue
@@ -0,0 +1,25 @@
+<template>
+  <hr>
+  <h2>Cafi</h2>
+  <p>
+    Kami adalah perusahaan Cafi yang memasarkan aneka produk coffee dan roti panggang kesukaan semua orang. Kami bergerak sejak tahun 2018.
+  </p>
+  <ul>
+    <li><a href="https://www.youtube.com/watchv=te-lFidcrcM">Lebih Lanjut</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'telkom',
+}
+</script>
+
+<style scoped>
+h2 {
+  font: sans-serif;
+  color: brown;
+}
+
+</style>
